c3f623fe937fe1263a4cc96e053bd9ab850ca203
Kuroneko33 <56042497+Kuroneko33@users.noreply.github.com>
2020-06-13 17:43:34 +0300

    Добавлены сущности В будущем планирую переделывать как минимум собщения, но пока что будет так

---
+++ Domain/Entities/Friend.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    class Friend
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int FriendId { get; set; }
+    }
+}
---
+++ Domain/Entities/FriendRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    class FriendRequest
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int PossibleFiendId { get; set; }
+    }
+}
