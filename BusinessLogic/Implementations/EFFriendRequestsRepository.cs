87c7cd5fedddfb1b4b78a5833061c3a3225ba3a3
Kuroneko33 <56042497+Kuroneko33@users.noreply.github.com>
2020-06-13 22:47:24 +0300

    Создан репозиторий для заявок в друзья

---
+++ BusinessLogic/Implementations/EFFriendRequestsRepository.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLogic.Interfaces;
+using Domain;
+using Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLogic.Implementations
+{
+    public class EFFriendRequestsRepository : IFriendRequestsRepository
+    {
+        private EFDbContext context;
+        public EFFriendRequestsRepository(EFDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void AddFriendRequest(FriendRequest friendRequest)
+        {
+            context.FriendRequests.Add(friendRequest);
+            context.SaveChanges();
+        }
+
+        public void DeleteFriendRequest(FriendRequest friendRequest)
+        {
+            context.FriendRequests.Remove(friendRequest);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<FriendRequest> GetFriendRequests()
+        {
+            return context.FriendRequests;
+        }
+
+        public bool RequestIsSent(int userFromId, int userToId)
+        {
+            return context.FriendRequests.Count(x => x.UserId == userFromId && x.PossibleFiendId == userToId) != 0;
+        }
+    }
+}
---
+++ BusinessLogic/Interfaces/IFriendRequestsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IFriendRequestsRepository
+    {
+        IEnumerable<FriendRequest> GetFriendRequests();
+        bool RequestIsSent(int userFromId, int userToId);
+        void AddFriendRequest(FriendRequest friendRequest);
+        void DeleteFriendRequest(FriendRequest friendRequest);
+    }
+}
