cd5d2b2417d6bd18d82949a4f03b161cef56ee59
Kuroneko33 <56042497+Kuroneko33@users.noreply.github.com>
2020-06-13 23:04:06 +0300

    Создан репозиторий для сообщений

---
+++ BusinessLogic/Interfaces/IMessagesRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IMessagesRepository
+    {
+        #region Входящие сообщения
+        IEnumerable<IncomingMessage> GetIncomingMessages();
+        IEnumerable<IncomingMessage> GetIncomingMessagesByUserId(int userId);
+        void SaveIncomingMessage(IncomingMessage message);
+        void DeleteIncomingMessage(IncomingMessage message);
+        #endregion
+
+        #region Исходящие сообщения
+        IEnumerable<OutgoingMessage> GetOutgoingMessages();
+        IEnumerable<OutgoingMessage> GetOutgoingMessagesByUserId(int userId);
+        void SaveOutgoingMessage(OutgoingMessage message);
+        void DeleteOutgoingMessage(OutgoingMessage message);
+        #endregion
+    }
+}
