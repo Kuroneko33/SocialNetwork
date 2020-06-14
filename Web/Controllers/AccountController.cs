3ffed7cbf60a466831de3f3e764c5872691336aa
Kuroneko33 <56042497+Kuroneko33@users.noreply.github.com>
2020-06-14 05:15:36 +0300

    Добавлена регистрация и авторизация пользователей

---
+++ Web/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private DataManager dataManager;
+        public AccountController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (dataManager.MembershipProvider.ValidateUser(model.UserName, model.Passwprd))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Неудачная попытка входа на сайт");
+            }
+            return View(model);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                MembershipCreateStatus status = dataManager.MembershipProvider.CreateUser(
+                    model.UsrtName,
+                    model.Password,
+                    model.Email,
+                    model.FirstName,
+                    model.LastName,
+                    model.MiddleName);
+                if (status == MembershipCreateStatus.Success)
+                    return View("Success");
+                ModelState.AddModelError("", GetMembershipCreateStatusResultText(status));
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+
+        private string GetMembershipCreateStatusResultText(MembershipCreateStatus status)
+        {
+            if (status == MembershipCreateStatus.DuplicateEmail)
+                return "Пользователь с таким email уже существует";
+            if (status == MembershipCreateStatus.DuplicateUserName)
+                return "Пользователь с таким именем уже существует";
+            return "Неизвестная ошибка";
+        }
+    }
+}
---
+++ Web/Models/RegisterViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "*")]
+        public string UsrtName { get; set; }
+    
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароль подтверждён неверно")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [RegularExpression(@"^[a-zA-Z0-9.-]{1,20}@[a-zA-Z0-9]{1,20}\.[A-Za-z]{2,4}", ErrorMessage = "Неверный формат Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [RegularExpression(@"^[a-zA-Z0-9.-]{1,20}@[a-zA-Z0-9]{1,20}\.[A-Za-z]{2,4}", ErrorMessage = "Неверный формат Email")]
+        [Compare("Email", ErrorMessage = "Email подтверждён неверно")]
+        public string ConfirmEmail { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string MiddleName { get; set; }
+    }
+}
